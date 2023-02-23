216365e5afcb16309dd7245c5d6462d80daaeb1c
Marc Prange <marc.prange@gmx.de>
2023-02-23 11:12:22 +0100

    Add batch script.
    Starts job to calculate moist layer characteristics using
    calc_eml_chars_for_monsoon.py.

---
+++ batch_moist_layer_calc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#SBATCH -J moist_layers           # Specify job name
+#SBATCH -p compute          # Use partition shared
+#SBATCH -N 1               # Specify number of nodes (1 for serial applications!)
+#SBATCH -n 1               # Specify max. number of tasks to be invoked
+#SBATCH -t 08:00:00        # Set a limit on the total run time
+#SBATCH --mem=0            # 0=All memory 
+#SBATCH -A um0878          # Charge resources on this project account
+#SBATCH --output=%x_%A_%a.o   # File name for standard output
+#SBATCH --error=%x_%A_%a.err   # File name for error output
+
+module load python3
+
+~/.local/share/jupyter/kernels/bin/python calc_eml_chars_for_monsoon.py --cell_min=0 --cell_max=100
